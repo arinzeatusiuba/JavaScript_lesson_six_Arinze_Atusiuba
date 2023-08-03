06f5c0b63276b7e7efd5ce63853cf03cf7c7533b
Arinze Atusiuba <arinzeatusiuba@gmail.com>
2023-08-03 16:48:07 +0100

    JavaScript lesson six

---
+++ tasks/task_four.js
@@ -0,0 +1,20 @@
+//TASK QUESTION
+//Create an object with multiple properties.
+//Use the delete keyword to remove a specific property from the object.
+//Log the object to the console to see the updated properties.
+
+
+
+
+
+const myBio = {
+  name: 'arinze',
+  age: 20,
+  sex: 'male'
+};
+
+// I UseD the delete keyword to remove a specific property (e.g., 'age') from the object
+delete myBio.age;
+
+
+console.log(myBio);
